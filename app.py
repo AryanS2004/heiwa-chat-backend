b85e1d76d2cb7cbc0866dd172b285e98336a4122
ARYAN SHARMA <aryaleena2004@gmail.com>
2025-05-03 11:28:02 +0530

    Initial commit

--- app.py
+++ app.py
@@ -56,4 +56,9 @@ def clean_text(text):
     return text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Required by Render
+    app.run(debug=True, host='0.0.0.0', port=port)
+
